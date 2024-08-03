f7a8fc7acdad0bd6d3b53307698ad592967f6d7b
Trishit Debsharma <trishit_debsharma@Trishits-MacBook-Air.local>
2024-08-03 22:22:29 +0530

    changed debug to False and added requirements.txt

--- tds/settings.py
+++ tds/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(d3nl83qyx$1tbkb#3x5x_^tpo(_zbe9^8%y-lq%-@+fetrurh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrer.com', '127.0.0.1']
 
 
 # Application definition
